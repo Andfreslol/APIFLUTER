c2403e0be351ca603b5fc90bb56c2a350b70cae4
Andres <carlosandresacmil@gmail.com>
2026-07-24 01:13:03 -0500

    backs config

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "postgresql://cruduser:vJHFuJzFHpsoDsc4Z1IbSN0ULNaAX3IH@dpg-d9hfqtupbkes73a0276g-a.oregon-postgres.render.com/crudapi_qaac"
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
